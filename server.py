a9d6db52304b7d7249fe760d6256e6de89e16436
romanyakovlev <yakovlevroman8@gmail.com>
2017-01-20 02:05:51 +0700

    add ajax functionality

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+def format_text(original_text):
+    #Logic for text formatting
+    formatted_text = original_text
+    return formatted_text
+
+@app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    if request.method == 'GET':
+        return render_template('form.html')
+    if request.method == 'POST':
+        data_json = request.get_json()
+        original_text = data_json['text_string']
+        formatted_text = format_text(original_text)
+        return jsonify(formatted_text=formatted_text)
 
 if __name__ == "__main__":
     app.run()
