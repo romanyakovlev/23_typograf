2260aacc186b76278f6864bba9ee5788cc36d315
Roman Yakovlev <yakovlevroman8@gmail.com>
2017-01-21 22:32:24 +0700

    Update typograf.py

--- typograf.py
+++ typograf.py
@@ -5,15 +5,15 @@ def compile_patterns():
     patterns_and_replacements = [
         # replace single and double quotes
         (re.compile(r'''("|')(.+)\1''', re.DOTALL), r'«\2»'),
-        # replace hyphen on short dash between numbers
+        # replace hyphen to short dash between numbers
         (re.compile(r'(\d+)‐(\d+)'), r'\1&ndash;\2'),
-        # replace simple hyphen on non breaking hyphen between number and word
+        # replace simple hyphen to non breaking hyphen between number and word
         (re.compile(r'(\d+)‐(\w+)'), r'\1&#8209;\2'),
         # remove extra spaces and extra new lines
         (re.compile(r'(\r\n|\n|\s){1}\1+'), r'\1'),
         # add non-breaking space between
         (re.compile(r'(\w{1,2}) (\w{3,})'), r'\1&nbsp;\2'),
-        # replace hyphen on dash between spaces
+        # replace hyphen to dash between spaces
         (re.compile(r'\s-\s'), r' — ')
     ]
     return patterns_and_replacements
