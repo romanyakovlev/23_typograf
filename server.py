9dfc49ab5c86402672537bd587baf9583f9a6a2a
romanyakovlev <yakovlevroman8@gmail.com>
2017-03-21 23:34:11 +0700

    remove json data, changed to plain text

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
-import re
-
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, abort
 from typograf import format_text
 
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def start_page():
     if request.method == 'GET':
         return render_template('form.html')
     if request.method == 'POST':
-        data_json = request.get_json()
-        original_text = data_json['text_string']
+        original_text = request.form['text']
+        if type(original_text) != str:
+            abort(403)
         formatted_text = format_text(original_text)
-        return jsonify(formatted_text=formatted_text)
+        return formatted_text
 
 
 if __name__ == "__main__":
--- typograf.py
+++ typograf.py
@@ -1,25 +1,24 @@
 import re
 
 
-def compile_patterns():
-    patterns_and_replacements = [
-        # replace single and double quotes
-        (re.compile(r'''("|')(.+)\1''', re.DOTALL), r'«\2»'),
-        # replace hyphen to short dash between numbers
-        (re.compile(r'(\d+)‐(\d+)'), r'\1&ndash;\2'),
-        # replace simple hyphen to non breaking hyphen between number and word
-        (re.compile(r'(\d+)‐(\w+)'), r'\1&#8209;\2'),
-        # remove extra spaces and extra new lines
-        (re.compile(r'(\r\n|\n|\s){1}\1+'), r'\1'),
-        # add non-breaking space between
-        (re.compile(r'(\w{1,2}) (\w{3,})'), r'\1&nbsp;\2'),
-        # replace hyphen to dash between spaces
-        (re.compile(r'\s-\s'), r' — ')
-    ]
-    return patterns_and_replacements
+PATTERNS_AND_REPLACEMENTS = [
+    # replace single and double quotes
+    (re.compile(r'''("|')(.+)\1''', re.DOTALL), r'«\2»'),
+    # replace hyphen to short dash between numbers
+    (re.compile(r'(\d+)‐(\d+)'), r'\1&ndash;\2'),
+    # replace simple hyphen to non breaking hyphen between number and word
+    (re.compile(r'(\d+)‐(\w+)'), r'\1&#8209;\2'),
+    # remove extra spaces and extra new lines
+    (re.compile(r'(\r\n|\n|\s){1}\1+'), r'\1'),
+    # add non-breaking space between
+    (re.compile(r'(\w{1,2}) (\w{3,})'), r'\1&nbsp;\2'),
+    # replace hyphen to dash between spaces
+    (re.compile(r'\s-\s'), r' — ')
+]
+
 
 def format_text(formatted_text):
-    patterns_and_replacements = compile_patterns()
-    for pattern, replacement in patterns_and_replacements:
+    global PATTERNS_AND_REPLACEMENTS
+    for pattern, replacement in PATTERNS_AND_REPLACEMENTS:
         formatted_text = re.sub(pattern, replacement, formatted_text)
     return formatted_text
